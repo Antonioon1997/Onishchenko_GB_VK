ebc88284c2fef8c9479c2ac2ed8f58873a5b71ba
Anton <good-price0@mail.ru>
2021-02-13 19:28:06 +0300

    Added some elements

--- Onishchenko_GB_VK/Onishchenko_GB_VK/ViewController.swift
+++ Onishchenko_GB_VK/Onishchenko_GB_VK/ViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var background: UIView!
+    @IBOutlet var pugBackground: UIView!
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.background.backgroundColor = UIColor(red: 240.0/256.0, green: 128.0/256.0, blue: 128.0/256.0, alpha: 1)
+        self.pugBackground.backgroundColor = UIColor(red: 210.0/256.0, green: 128.0/256.0, blue: 128.0/256.0, alpha: 1)
+        self.loginTextField.backgroundColor = UIColor(red: 210.0/256.0, green: 128.0/256.0, blue: 128.0/256.0, alpha: 1)
+        
+        self.passwordTextField.clearsOnBeginEditing = true
+        self.passwordTextField.isSecureTextEntry = true
+        self.passwordTextField.backgroundColor = UIColor(red: 210.0/256.0, green: 128.0/256.0, blue: 128.0/256.0, alpha: 1)
+        
+        self.loginButton.showsTouchWhenHighlighted = true
+        self.loginButton.layer.cornerRadius = 5
     }
 
 
